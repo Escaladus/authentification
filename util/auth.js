ee04d6a0dff55c92a1cfdead2f681cec439829b1
Denis Eskelinen <escaladus@gmail.com>
2023-07-31 20:21:35 +0300

    connected to authenticator

--- util/auth.js
+++ util/auth.js
@@ -2,12 +2,33 @@ import axios from "axios";
 import { SECURED_INFORMATION } from "../protect/secure_data";
 
 const API_KEY = SECURED_INFORMATION.YOUR_OWN_API_KEY;
-const URL = SECURED_INFORMATION.YOUR_OWN_LINK;
+const YOUR_LINK = SECURED_INFORMATION.YOUR_LINK;
 
-async function createUser(email, password) {
-  const response = await axios.post(URL + API_KEY, {
+async function authenticate(mode, email, password) {
+  const url = `${YOUR_LINK}${mode}?key=${API_KEY}`;
+  const response = await axios.post(url, {
     email: email,
     password: password,
     returnSecureToken: true,
   });
+  console.log(response.data);
 }
+
+export async function createUser(email, password) {
+  await authenticate("signUp", email, password);
+}
+
+export async function login(email, password) {
+  await authenticate("signInWithPassword", email, password);
+}
+
+/* 
+  
+  // createUser...
+  const response = await axios.post(URL_SIGN_UP + API_KEY, {
+    email: email,
+    password: password,
+    returnSecureToken: true,
+  });
+
+  */
